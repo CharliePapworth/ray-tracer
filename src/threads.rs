f48747610c15a4cd4879a9fdeef5324fc7d51b5f
Charlie <charliepapworth@protonmail.com>
2022-02-22 17:02:32 +0000

    Moved threading functions into their own file

---
+++ src/threads.rs
@@ -0,0 +1,99 @@
+use crate::*;
+use crate::vec::*;
+use crate::traceable::*;
+use crate::util::*;
+
+use std::sync::Arc;
+use std::sync::Barrier;
+use std::sync::mpsc::*;
+use std::thread;
+
+#[derive (Copy, Clone)]
+pub struct InputData {
+    pub image_width: usize,
+    pub image_height: usize,
+    pub samples_per_pixel: i32,
+    pub max_depth: i32,
+    pub run: bool
+}
+
+#[derive (Clone, Default)]
+pub struct ImageData{
+    pub pixel_colors: Vec<Color>,
+    pub image_width: usize,
+    pub image_height: usize,
+    pub samples: usize
+}
+
+pub fn initialise_threads<H>(input_data: InputData, scene_data: Arc<StaticData<H>>, samples: i32, remaining_samples: i32, thread_to_gui_tx: Sender<ImageData>, num_threads: i32) -> Vec<Sender<InputData>>
+where H: Hit + 'static {
+    let mut senders = vec![];
+    let barrier = Arc::new(Barrier::new((num_threads) as usize));
+    for i in 0..num_threads {
+        let barrier_clone = Arc::clone(&barrier);
+        let thread_to_gui_tx = thread_to_gui_tx.clone();
+        let (gui_to_thread_tx, gui_to_thread_rx): (Sender<InputData>, Receiver<InputData>) = channel();
+        let static_data = Arc::clone(&scene_data);
+        if i == num_threads{
+            thread::spawn(move || run_thread(input_data, static_data, remaining_samples, thread_to_gui_tx,  gui_to_thread_rx, barrier_clone));
+
+        } else {
+            thread::spawn(move || run_thread(input_data, static_data, samples, thread_to_gui_tx, gui_to_thread_rx,  barrier_clone));
+        }
+
+        senders.push(gui_to_thread_tx);
+    }
+    senders
+}
+
+ pub fn run_thread<H>(mut input_data: InputData, static_data: Arc<StaticData<H>>, samples: i32, thread_to_gui_tx: Sender<ImageData>, gui_to_thread_rx: Receiver<InputData>, barrier: Arc<Barrier>)
+ where H: Hit + 'static {
+
+    while input_data.run {
+        barrier.wait();
+        input_data = iterate_image(input_data, Arc::clone(&static_data), samples, thread_to_gui_tx.clone() , &gui_to_thread_rx);
+    }
+ }
+
+pub fn iterate_image<H>(mut input_data: InputData, static_data: Arc<StaticData<H>>, samples: i32, thread_to_gui_tx: Sender<ImageData>, gui_to_thread_rx: &Receiver<InputData>)
+ -> InputData where H: Hit + 'static {
+
+    let image_height = input_data.image_height;
+    let image_width = input_data.image_width;
+    for _ in 0..samples{
+        let mut pixel_colors = vec![Color::new(0.0,0.0,0.0); image_height * image_width];
+        for j in 0..image_height{
+            for i in 0..image_width{
+                    let u = (rand_double(0.0, 1.0) + i as f64)/(image_width as f64 - 1.0);
+                    let v = (rand_double(0.0, 1.0) + (image_width - j) as f64)/((image_width - 1) as f64);
+                    let r = static_data.cam.get_ray(u,v);
+                    let pixel_index = (j*image_width + i) as usize;
+                    pixel_colors[pixel_index] = pixel_colors[pixel_index] + ray_color(&r, static_data.background, &static_data.world, input_data.max_depth);
+                    let message = gui_to_thread_rx.try_recv();
+                    match message {
+                        Ok(input_data) => return input_data,
+                        Err(err) => {
+                            match err {
+                                TryRecvError::Empty => {}
+                                TryRecvError::Disconnected => {
+                                    input_data.run = false;
+                                    return input_data;
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+        }
+        let output = ImageData{pixel_colors, image_width, image_height, samples: 1};
+        let send_result = thread_to_gui_tx.send(output);
+        match send_result {
+                Err(_) => {
+                    input_data.run = false;
+                    return input_data;
+                }
+                Ok(_) => {}
+            }
+        }
+    input_data 
+}
