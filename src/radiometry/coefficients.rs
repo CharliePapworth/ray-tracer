40ae4abdb1056fc97ef002c5e35026e37175dc85
Charlie <charliepapworth@protonmail.com>
2022-05-03 14:10:27 +0100

    Refactored coefficients.rs to use const generics

--- src/radiometry/coefficients.rs
+++ src/radiometry/coefficients.rs
@@ -1,56 +1,62 @@
-use std::ops::{Index, IndexMut, Neg};
+use std::ops::{Index, IndexMut, Neg, Add, Sub, Mul, AddAssign, SubAssign, Div};
 use std::iter::zip;
 use std::ops;
 
 use crate::util::bound_f32;
 
-const COEFFICIENTCOUNT: usize = 60;
-
 /// Infrastructure underpining the spectrum implementations. Each implementation contains an array of coefficients, on
 /// which they need to make some common operations. 
-pub struct Coefficients {
-    arr: [f32; COEFFICIENTCOUNT],
+pub struct Coefficients<const T: usize> {
+    arr: [f32; T],
 }
 
-impl Coefficients {
-    pub fn new(constant: f32) -> Coefficients {
-        let coefficients = [constant; COEFFICIENTCOUNT];
+impl<const T: usize> Coefficients<T> {
+    pub fn new(constant: f32) -> Coefficients<T> {
+        let coefficients = [constant; T];
         Coefficients { arr: coefficients } 
     }
 
-    fn elementwise_binary_operation(&self, rhs: &Coefficients, operation: fn(f32, f32) -> f32) -> Coefficients {
-        let mut coefficients = [0f32; COEFFICIENTCOUNT];
-        for i in 0..COEFFICIENTCOUNT {
+    fn elementwise_binary_operation(&self, rhs: &Coefficients<T>, operation: fn(f32, f32) -> f32) -> Coefficients<T> {
+        let mut coefficients = [0f32; T];
+        for i in 0..T {
             coefficients[i] = operation(self.arr[i], rhs.arr[i]);
         }
         Coefficients { arr: coefficients } 
     }
 
-    fn elementwise_binary_operation_in_place(&mut self, rhs: &Coefficients, operation: fn(f32, f32) -> f32) {
-        for i in 0..COEFFICIENTCOUNT {
+    fn elementwise_binary_operation_in_place(&mut self, rhs: &Coefficients<T>, operation: fn(f32, f32) -> f32) {
+        for i in 0..T {
             self.arr[i] = operation(self.arr[i], rhs.arr[i]);
         }
     }
 
     fn unary_operation_in_place<F>(&mut self, operation: F) where F: Fn(f32) -> f32 {
-        for i in 0..COEFFICIENTCOUNT {
+        for i in 0..T {
             self.arr[i] = operation(self.arr[i]);
         }
     }
 
-    pub fn powi(&self, exponent: i32) -> Coefficients {
+    pub fn elementwise_multiplication(&self, other: &Coefficients<T>) {
+        self.elementwise_binary_operation(&other, |a, b| a * b)
+    }
+
+    pub fn elementwise_division(&self, other: &Coefficients<T>) {
+        self.elementwise_binary_operation(&other, |a, b| a * b)
+    }
+
+    pub fn powi(&self, exponent: i32) -> Coefficients<T> {
         Coefficients { arr: self.arr.map(|a| a.powi(exponent)) }
     }
 
-    pub fn exp(&self) -> Coefficients {
+    pub fn exp(&self) -> Coefficients<T> {
         Coefficients { arr: self.arr.map(|a| a.exp()) }
     }
 
-    pub fn sqrt(&self) -> Coefficients {
+    pub fn sqrt(&self) -> Coefficients<T> {
         Coefficients { arr: self.arr.map(|a| a.sqrt()) }
     }
 
-    pub fn lerp(&self, other: &Coefficients, t: f32) -> Coefficients {
+    pub fn lerp(&self, other: &Coefficients<T>, t: f32) -> Coefficients<T> {
         (1.0 - t) * self + t * other
     }
 
@@ -62,25 +68,26 @@ impl Coefficients {
         self.arr.iter().any(|a| a.is_nan())
     }
 
-    pub fn clamp(&self, min: f32, max: f32) -> Coefficients {
+    pub fn clamp(&self, min: f32, max: f32) -> Coefficients<T> {
         Coefficients{ arr: self.arr.map(|a| bound_f32(a, min, max)) }
     }
 }
 
-impl Index<usize> for Coefficients {
+impl<const T: usize> Index<usize> for Coefficients<T> {
     type Output = f32;
     fn index(&self, index: usize) -> &Self::Output {
         &self.arr[index]
     }
 }
 
-impl IndexMut<usize> for Coefficients {
+impl<const T: usize> IndexMut<usize> for Coefficients<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.arr[index]
     }
 }
 
-impl Neg for Coefficients {
+// Vector Operations
+impl<const T: usize> Neg for Coefficients<T> {
     type Output = Self;
 
     fn neg(self) -> Self {
@@ -88,52 +95,156 @@ impl Neg for Coefficients {
     }
 }
 
-impl_op_ex!(+ |lhs: &Coefficients, rhs: &Coefficients| -> Coefficients {
-        lhs.elementwise_binary_operation(&rhs, |a, b| a + b)
-    }
-);
+impl<const T: usize> Add for Coefficients<T> {
+    type Output = Coefficients<T>;
 
-impl_op_ex!(+= |lhs: &mut Coefficients, rhs: &Coefficients| {
-        lhs.elementwise_binary_operation_in_place(&rhs, |a, b| a + b);
+    fn add(self, other: Coefficients<T>) -> Coefficients<T> {
+        self.elementwise_binary_operation(&other, |a, b| a + b)
     }
-);
+}
 
-impl_op_ex!(- |lhs: &Coefficients, rhs: &Coefficients| -> Coefficients {
-        lhs.elementwise_binary_operation(&rhs, |a, b| a - b)
-    }
-);
+impl<const T: usize> Add for &Coefficients<T> {
+    type Output = Coefficients<T>;
 
-impl_op_ex!(-= |lhs: &mut Coefficients, rhs: Coefficients| {
-        lhs.elementwise_binary_operation_in_place(&rhs, |a, b| a - b);
+    fn add(self, other: &Coefficients<T>) -> Coefficients<T> {
+        self.elementwise_binary_operation(&other, |a, b| a + b)
     }
-);
+}
 
-impl_op_ex!(* |lhs: &Coefficients, rhs: &Coefficients| -> Coefficients {
-        lhs.elementwise_binary_operation(&rhs, |a, b| a * b)
-    }
-);
+impl<const T: usize> Add for Coefficients<T> {
+    type Output = Coefficients<T>;
 
-impl_op_ex!(*= |lhs: &mut Coefficients, rhs: Coefficients| {
-        lhs.elementwise_binary_operation_in_place(&rhs, |a, b| a * b);
+    fn add(&self, other: &Coefficients<T>) -> Coefficients<T> {
+        self.elementwise_binary_operation(&other, |a, b| a + b)
     }
-);
+}
 
-impl_op_ex_commutative!(* |lhs: &Coefficients, rhs: &f32| -> Coefficients {
-        Coefficients { arr: lhs.arr.map(|a| a * rhs) }
-    }
-);
+impl<const T: usize> Add for &Coefficients<T> {
+    type Output = Coefficients<T>;
 
-impl_op_ex!(*= |lhs: &mut Coefficients, rhs: &f32| {
-        lhs.unary_operation_in_place(|a| a * rhs);
+    fn add(&self, other: &Coefficients<T>) -> Coefficients<T> {
+        self.elementwise_binary_operation(&other, |a, b| a + b)
     }
-);
+}
 
-impl_op_ex!(/ |lhs: &Coefficients, rhs: &Coefficients| -> Coefficients {
-        lhs.elementwise_binary_operation(&rhs, |a, b| a / b)
+impl<const T: usize> AddAssign for Coefficients<T> {
+    fn add_assign(&mut self, other: Self) {
+        self.elementwise_binary_operation_in_place(&other, |a, b| a + b)
     }
-);
+}
 
-impl_op_ex!(/= |lhs: &mut Coefficients, rhs: &Coefficients| {
-        lhs.elementwise_binary_operation_in_place(&rhs, |a, b| a / b);
+impl<const T: usize> AddAssign for &Coefficients<T> {
+    fn add_assign(&mut self, other: Self) {
+        self.elementwise_binary_operation_in_place(&other, |a, b| a + b)
     }
-);
+}
+
+impl<const T: usize> Sub for Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn sub(self, other: Coefficients<T>) -> Coefficients<T> {
+        self.elementwise_binary_operation(&other, |a, b| a - b)
+    }
+}
+
+impl<const T: usize> Sub for &Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn sub(self, other: &Coefficients<T>) -> Coefficients<T> {
+        self.elementwise_binary_operation(&other, |a, b| a - b)
+    }
+}
+
+impl<const T: usize> Sub for Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn sub(&self, other: &Coefficients<T>) -> Coefficients<T> {
+        self.elementwise_binary_operation(&other, |a, b| a - b)
+    }
+}
+
+impl<const T: usize> Sub for &Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn sub(&self, other: &Coefficients<T>) -> Coefficients<T> {
+        self.elementwise_binary_operation(&other, |a, b| a - b)
+    }
+}
+
+impl<const T: usize> SubAssign for Coefficients<T> {
+    fn sub_assign(&mut self, other: Self) {
+        self.elementwise_binary_operation_in_place(&other, |a, b| a - b)
+    }
+}
+
+impl<const T: usize> SubAssign for &Coefficients<T> {
+    fn sub_assign(&mut self, other: Self) {
+        self.elementwise_binary_operation_in_place(&other, |a, b| a - b)
+    }
+}
+
+
+// Scalar Operations
+impl<const T: usize> Mul<f32> for Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn mul(self, scalar: f32) -> Coefficients<T> {
+        self.arr.map(|a| a * scalar)
+    }
+}
+
+impl<const T: usize> Mul<f32> for &Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn mul(&self, scalar: f32) -> Coefficients<T> {
+        self.arr.map(|a| a * scalar)
+    }
+}
+
+impl<const T: usize> Mul<&f32> for Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn mul(self, scalar: &f32) -> Coefficients<T> {
+        self.arr.map(|a| a * scalar)
+    }
+}
+
+impl<const T: usize> Mul<&f32> for &Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn mul(&self, scalar: &f32) -> Coefficients<T> {
+        self.arr.map(|a| a * scalar)
+    }
+}
+
+impl<const T: usize> Div<f32> for Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn div(self, scalar: f32) -> Coefficients<T> {
+        self.arr.map(|a| a / scalar)
+    }
+}
+
+impl<const T: usize> Div<f32> for &Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn div(&self, scalar: f32) -> Coefficients<T> {
+        self.arr.map(|a| a / scalar)
+    }
+}
+
+impl<const T: usize> Div<&f32> for Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn div(self, scalar: &f32) -> Coefficients<T> {
+        self.arr.map(|a| a * scalar)
+    }
+}
+
+impl<const T: usize> Div<&f32> for &Coefficients<T> {
+    type Output = Coefficients<T>;
+
+    fn div(&self, scalar: &f32) -> Coefficients<T> {
+        self.arr.map(|a| a * scalar)
+    }
+}
