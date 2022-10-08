1f09e5b611203886d344ab68b8305e1e3efc245c
Charlie Papworth <charliepapworth@protonmail.com>
2022-10-08 12:11:37 +0100

    FilmTile now uses Point2 to specify tile corners

--- src/film.rs
+++ src/film.rs
@@ -1,3 +1,5 @@
+use nalgebra::Point2;
+
 use crate::{filter::Filter, image::Color, spectrum::Spectrum};
 
 #[derive(Clone, Default)]
@@ -44,8 +46,8 @@ impl Film {
                     column_pixels = pixels_per_final_column;
                 }
 
-                let bottom_left = (i * row_pixels, j * column_pixels);
-                let top_right = ((i + 1) * row_pixels, (j + 1) * column_pixels);
+                let bottom_left = Point2::<usize>::new(i * row_pixels, j * column_pixels);
+                let top_right = Point2::<usize>::new((i + 1) * row_pixels, (j + 1) * column_pixels);
                 let tile = FilmTile::new(bottom_left, top_right, 0, 0);
                 tiles.push(tile);
             }
@@ -57,15 +59,15 @@ impl Film {
     pub fn merge_tile(&self, tile: &FilmTile) {
         //All pixels in a given tile have the same id. If the id of the tile is not greater than the id of any of the corresponding
         //pixels in the film, then no update to the film needs to occur.
-        let test_pixel_index = self.get_index(tile.bottom_left.0, tile.bottom_left.1);
+        let test_pixel_index = self.get_index(tile.bottom_left.x, tile.bottom_left.y);
         let test_pixel_id = self.pixels[test_pixel_index].id;
         if test_pixel_id < tile.id {
             return;
         }
 
         let mut tile_index = 0;
-        for row in tile.bottom_left.0..tile.top_right.0 {
-            for column in tile.bottom_left.1..tile.top_right.1 {
+        for row in tile.bottom_left.x..tile.top_right.x {
+            for column in tile.bottom_left.y..tile.top_right.y {
                 let film_index = self.get_index(row, column);
                 self.pixels[film_index] = tile.pixels[tile_index];
                 tile_index += 1;
@@ -79,9 +81,9 @@ impl Film {
 #[derive(Clone, Default)]
 pub struct FilmTile {
     /// The (x, y) coordinates corresponding to the bottom-left of the tile.
-    pub bottom_left: (usize, usize),
+    pub bottom_left: Point2<usize>,
     /// The (x, y) coordinates corresponding to the top-right of the tile.
-    pub top_right: (usize, usize),
+    pub top_right: Point2<usize>,
     /// FilmTile pixels are stored from bottom to top, left to right.
     pub pixels: Vec<FilmPixel>,
     /// Counts the number of samples integrated into the film tile.
@@ -93,12 +95,12 @@ pub struct FilmTile {
 
 impl FilmTile {
     pub fn new(
-        bottom_left: (usize, usize),
-        top_right: (usize, usize),
+        bottom_left: Point2<usize>,
+        top_right: Point2<usize>,
         samples: i32,
         id: i32,
     ) -> FilmTile {
-        let number_of_pixels = (top_right.0 - bottom_left.0) * (top_right.1 - bottom_left.1);
+        let number_of_pixels = (top_right.x - bottom_left.x) * (top_right.y - bottom_left.y);
         let film_pixels = vec![FilmPixel::default(); number_of_pixels];
         FilmTile {
             bottom_left,
