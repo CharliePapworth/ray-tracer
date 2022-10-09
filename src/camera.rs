31dedf41d28542fc6d4e0d17ff7090bfc3ce7376
Charlie Papworth <charliepapworth@protonmail.com>
2022-10-09 23:18:21 +0100

    Refined comments Completed screen_to_raster() method

--- src/camera.rs
+++ src/camera.rs
@@ -4,9 +4,12 @@ use crate::raytracing::Ray;
 use crate::sampler;
 use crate::util::deg_to_rad;
 use core::f32;
-use nalgebra::{Matrix4, Perspective3, Translation3, Unit};
+use nalgebra::{Matrix4, Perspective3, Translation3, Unit, Point2};
 use std::f32::consts::PI;
 
+/// Type representing a perspective camera. Generates rays for use in the integrator.
+/// # Further Reading
+/// https://pbr-book.org/3ed-2018/Camera_Models/Camera_Model#Camera
 #[derive(Clone)]
 pub struct Camera {
     /// The camera_to_world transformation matrix projects points in camera space into world space.
@@ -14,8 +17,14 @@ pub struct Camera {
     /// direction, and the `y`-axis points vertically upwards.
     camera_to_world: Matrix4<f32>,
     /// The perspective transformation matrix projects points in camera space into screen space.
-    /// The `x'` and `y'` coordinates of the projected points are equal to the unprojected `x` and `y` coordinates divided by the `z` coordinate.
-    /// The projected `z'` coordinate is computed so that points on the near plane map to `z' = 0` and points on the far plane map to `z = 1`.
+    /// The `x` and `y` coordinates are first  divided by the `z` coordinate.
+    /// 
+    /// If the image is square, they're then rescaled such that the `x'` and `y'` coordinates
+    /// for all points within the field of view lie between `[-1, 1]`. Otherwise, the direction in which the image is narrower maps to `[-1, 1]`, and the wider
+    /// direction maps to a proportionally larger range of screen space values. 
+    /// 
+    /// The projected `z'` coordinate is computed so that points on the near plane map 
+    /// to `z' = 0` and points on the far plane map to `z' = 1`.
     camera_to_screen: Matrix4<f32>,
     /// A screen_to_raster transformation matrix projects points in camera space into raster space. In raster space, depth values are the same as in screen
     /// space, but the origin is in the upper-left hand corner of the image. The bottom right hand corner is defined as `(image_width, image_height)`,
@@ -55,8 +64,7 @@ impl Camera {
     /// * `camera_to_world` - A transformation from camera space to world space.
     /// * `film` - The film the camera writes to.
     ///  * `focus_dist` - The focus distance of the camera.
-    /// # Further Reading
-    /// Camera space definitions: https://pbr-book.org/3ed-2018/Camera_Models/Camera_Model#Camera
+
     pub fn new(camera_to_world: Matrix4<f32>, film: Film, focus_dist: f32) {}
 
     pub fn calculate_camera_to_screen(
@@ -73,15 +81,19 @@ impl Camera {
                                                       0.0, 0.0, m33,  m34,
                                                       0.0, 0.0, 1.0, 0.0);
 
-        let inverse_tan_angle = 1.0 / (field_of_view * PI / 180.0).tan() / 2.0;
-        let scale_vector = Vector3::new(inverse_tan_angle, inverse_tan_angle, 1.0);
+        let x_y_scale_factor = 1.0 / (field_of_view * PI / 180.0).tan() / 2.0;
+        let scale_vector = Vector3::new(x_y_scale_factor, x_y_scale_factor, 1.0);
         let scale = Matrix4::new_nonuniform_scaling(&scale_vector);
         perspective_transformation * scale
     }
 
-    pub fn calculate_screen_to_raster() {
-        // let translation = Translation3::new()
-        // let translation = Matrix4::new_translation(translation)
+    pub fn calculate_screen_to_raster(&self, screen_bottom_left_corner: Point2<f32>, screen_top_right_corner: Point2<f32>) -> Matrix4<f32> {
+        let screen_width = screen_top_right_corner.x - screen_bottom_left_corner.x;
+        let screen_height = screen_top_right_corner.y - screen_bottom_left_corner.y;
+        let scale_to_raster = Matrix4::new_nonuniform_scaling(&Vector3::new(self.film.image_width as f32, self.film.image_height as f32, 1.0));
+        let rescale_screen = Matrix4::new_nonuniform_scaling(&Vector3::new(1.0 / screen_width, 1.0 / screen_height, 1.0));
+        let translate = Matrix4::new_translation(&Vector3::new(-screen_bottom_left_corner.x, -screen_top_right_corner.y, 0.0));
+        scale_to_raster * rescale_screen * translate
     }
 
     pub fn get_ray(&self, s: f32, t: f32) -> Ray {
