71381d7e6a0d7e35a323c561a3f8c454d918ce92
Charlie Papworth <charliepapworth@protonmail.com>
2022-10-12 19:40:01 +0100

    renamed color.rs to rgb.rs

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,9 +1,9 @@
 pub mod camera;
 pub mod camera_sample;
 pub mod film;
-pub mod color;
+pub mod rgb;
 
 pub use camera::*;
 pub use camera_sample::*;
 pub use film::*;
-pub use color::*;
+pub use rgb::*;
